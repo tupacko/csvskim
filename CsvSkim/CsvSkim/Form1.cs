fce76ead8672decddf3a971fb1604a62cc8e0935
tupacko <tupacko@gmail.com>
2016-06-19 21:04:17 +0300

    Initial commit of the application.

---
+++ CsvSkim/CsvSkim/Form1.cs
@@ -0,0 +1,92 @@
+﻿using CsvSkim.Properties;
+using System;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace CsvSkim
+{
+    public partial class Form1 : Form
+    {
+        private readonly string file;
+        private readonly char separator;
+
+        public Form1(string file, char separator)
+        {
+            this.file = file;
+            this.separator = separator;
+
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MaximizeWindow();
+            LoadFile();
+        }
+
+        private void MaximizeWindow()
+        {
+            WindowState = FormWindowState.Maximized;
+        }
+
+        private void LoadFile()
+        {
+            try
+            {
+                using (StreamReader csvReader = new StreamReader(file))
+                {
+                    LoadLines(csvReader);
+                }
+            }
+            catch (IOException)
+            {
+                ShowFileNotFoundErrorMessage();
+            }
+        }
+
+        private void LoadLines(StreamReader csvReader)
+        {
+            string line = csvReader.ReadLine();
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                return;
+            }
+
+            InitializeColumns(line);
+
+            do
+            {
+                string[] row = SplitLine(line);
+                string[] processedRow = row.Select(CleanColumnValue).ToArray();
+
+                this.dataGridView1.Rows.Add(row);
+            } while ((line = csvReader.ReadLine()) != null);
+        }
+
+        private void InitializeColumns(string line)
+        {
+            string[] row = SplitLine(line);
+
+            for (int i = 0; i < row.Length; i++)
+            {
+                this.dataGridView1.Columns.Add("", "");
+            }
+        }
+
+        private string[] SplitLine(string line)
+        {
+            return line.Split(this.separator);
+        }
+
+        private string CleanColumnValue(string rawValue)
+        {
+            return rawValue.Trim().Trim('"');
+        }
+
+        private static void ShowFileNotFoundErrorMessage()
+        {
+            MessageBox.Show(Resources.FileNotFoundErrorMessage, Resources.ErrorText, MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
---
+++ CsvSkim/CsvSkim/Program.cs
@@ -0,0 +1,81 @@
+﻿using CsvSkim.Properties;
+using System;
+using System.Windows.Forms;
+
+namespace CsvSkim
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main(string[] args)
+        {
+            string file, potentialSeparator;
+            char separator;
+
+            ProcessArguments(args, out file, out potentialSeparator);
+            ValidateFile(file);
+            ValidateSeparator(potentialSeparator);
+            separator = char.Parse(potentialSeparator);
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1(file, separator));
+        }
+
+        private static void ProcessArguments(string[] args, out string file, out string potentialSeparator)
+        {
+            file = string.Empty;
+            potentialSeparator = ",";
+
+            if (0 == args.Length)
+            {
+                return;
+            }
+
+            if (1 == args.Length)
+            {
+                file = args[0];
+                return;
+            }
+
+            potentialSeparator = args[0];
+            file = args[1];
+        }
+
+        private static void ValidateFile(string file)
+        {
+            if (string.IsNullOrWhiteSpace(file))
+            {
+                ShowUsage();
+            }
+        }
+
+        private static void ValidateSeparator(string potentialSeparator)
+        {
+            if (string.IsNullOrWhiteSpace(potentialSeparator))
+            {
+                ShowInvalidSeparatorMessage();
+            }
+
+            if (1 < potentialSeparator.Length)
+            {
+                ShowInvalidSeparatorMessage();
+            }
+        }
+
+        private static void ShowUsage()
+        {
+            MessageBox.Show(Resources.UsageText);
+            Environment.Exit(1);
+        }
+
+        private static void ShowInvalidSeparatorMessage()
+        {
+            MessageBox.Show(Resources.InvalidSeparatorErrorMessage);
+            Environment.Exit(2);
+        }
+    }
+}
